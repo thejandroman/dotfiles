cbda35ef72a28dc962ec468a2539bdb58e892085
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-10-09 18:12:06 -0400

    Switch to base16-ocean theme

--- preload/theme.el
+++ preload/theme.el
@@ -2,8 +2,11 @@
 ;;; Code:
 ;;; Commentary:
 
-(setq prelude-theme 'sanityinc-tomorrow-eighties)
+;; (use-package base16-theme
+;;   :ensure t)
+
+(setq prelude-theme 'base16-ocean)
 (setq prelude-flyspell nil)
-;(setq prelude-whitespace nil)
+; (setq prelude-whitespace nil)
 
 ;;; theme.el ends here
