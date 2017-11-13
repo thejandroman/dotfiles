4c97c8dcf8a690c441f6440fbf9126e021fa5f02
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-11-12 20:36:01 -0500

    Add basic init

---
+++ init.el
@@ -0,0 +1,43 @@
+;;; init.el -- Summary
+;;; Code:
+;;; Commentary:
+
+(package-initialize)
+
+;; Set up load path
+(defvar settings-dir)
+(defvar site-lisp-dir)
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+(add-to-list 'load-path settings-dir)
+(add-to-list 'load-path site-lisp-dir)
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;; Are we on a mac?
+(defvar is-mac)
+(setq is-mac (equal system-type 'darwin))
+
+;; Set up things!
+(require 'setup-package)
+(require 'defaults)
+
+;; Add some mac defaults
+(when is-mac
+    (require 'mac-defaults))
+
+;; Add features
+(require 'theme)
+(require 'syntax-checking)
+(require 'vcs)
+(require 'markdown)
+(require 'ruby)
+
+;;; init.el ends here
