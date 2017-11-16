7be0a3b5185620be3d73efae24e0e534cf773732
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-11-16 09:52:09 -0500

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a1a966cf2e87be6a148158c79863440ba2e45aa06cc214341feafe5c6deca4f2" default)))
  '(package-selected-packages
    (quote
-    (whitespace-cleanup-mode smartparens web-mode js2-mode json-mode rbenv markdown-mode magit flycheck exec-path-from-shell use-package better-defaults))))
+    (darktooth-theme helm yaml-mode whitespace-cleanup-mode smartparens web-mode js2-mode json-mode rbenv markdown-mode magit flycheck exec-path-from-shell use-package better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
