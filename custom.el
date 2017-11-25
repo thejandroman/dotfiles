53ab462a9fc539a12c3947fd0a12bec2f53f361d
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-11-25 11:40:47 -0800

    Update custom

--- custom.el
+++ custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a1a966cf2e87be6a148158c79863440ba2e45aa06cc214341feafe5c6deca4f2" default)))
+    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "146061a7ceea4ccc75d975a3bb41432382f656c50b9989c7dc1a7bb6952f6eb4" "a1a966cf2e87be6a148158c79863440ba2e45aa06cc214341feafe5c6deca4f2" default)))
  '(package-selected-packages
    (quote
-    (flymake-puppet puppet-mode darktooth-theme helm yaml-mode whitespace-cleanup-mode smartparens web-mode js2-mode json-mode rbenv markdown-mode magit flycheck exec-path-from-shell use-package better-defaults))))
+    (color-theme-solarized flymake-puppet puppet-mode darktooth-theme helm yaml-mode whitespace-cleanup-mode smartparens web-mode js2-mode json-mode rbenv markdown-mode magit flycheck exec-path-from-shell use-package better-defaults)))
+ '(solarized-termcolors 256))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
