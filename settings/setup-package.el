77f010db516bf5c0a5c2bd5b29a22045c4750ab5
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-12-28 17:00:25 -0400

    Remove elpa

--- settings/setup-package.el
+++ settings/setup-package.el
@@ -6,7 +6,6 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
 ;; Setup use-package
