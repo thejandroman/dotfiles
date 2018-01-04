b8e28ceae33f9c784a2673cfcdc180eec851c942
Alejandro Figueroa <alejandrofigueroa@github.com>
2018-01-04 13:06:01 -0400

    Override markdown mode to use livedown for preview

--- settings/markdown.el
+++ settings/markdown.el
@@ -4,13 +4,22 @@
 
 ;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/personal/emacs-livedown"))
 
+(use-package markdown-mode
+  :ensure t)
+
 (use-package livedown
   :config
   (defvar livedown-autostart)
   (setq livedown-autostart t))
 
-(use-package markdown-mode
-  :ensure t)
+(defun-minor-mode livedown-mode
+  "Override markdown mode to use livedown for preview"
+  :lighter " livedown"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-c C-c p") 'livedown-preview)
+            map))
+
+(add-hook 'markdown-mode-hook 'livedown-mode)
 
 (provide 'markdown)
 
