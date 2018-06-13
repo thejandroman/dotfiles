f8b13f42758e34e74ffe766a71d248fc38f683ba
Alejandro Figueroa <alejandrofigueroa@github.com>
2018-06-13 11:50:58 -0400

    Add ability to change theme based on light sensor

--- settings/theme.el
+++ settings/theme.el
@@ -14,10 +14,43 @@
 (use-package zenburn-theme
   :ensure t)
 
+(use-package sunburn-theme
+  :ensure t)
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t)
+
 ;; (customize-set-variable 'solarized-termcolors 256)
 ;; (customize-set-variable 'frame-background-mode (quote light))
-(load-theme 'zenburn t)
+;; (load-theme 'sunburn t)
 
+(setq current-theme "init")
+(defconst light-theme 'sanityinc-tomorrow-day)
+(defconst dark-theme 'sanityinc-tomorrow-night)
+
+;; will apply a dark theme if the room is dark, and a light theme if the room is
+;; bright
+(defun change-theme-for-lighting ()
+  (let* ((current-light-sensor-reading
+          (string-to-number
+           (shell-command-to-string "/Users/thejandroman/bin/lmutracker")))
+         (lid-open
+          (string-to-number
+           (shell-command-to-string "/Users/thejandroman/bin/lid-open"))))
+    (if (eql lid-open 0)
+        (if (< current-light-sensor-reading 4000000)
+            (when (not (string-equal current-theme "dark"))
+              (load-theme dark-theme 1)
+              (setq current-theme "dark"))
+          (when (not (string-equal current-theme "light"))
+            (load-theme light-theme 1)
+            (setq current-theme "light")))
+      (when (not (string-equal current-theme "dark"))
+        (load-theme dark-theme 1)
+        (setq current-theme "dark")))))
+
+;; probably want to run this less frequently than every second
+(run-at-time 1 1 #'change-theme-for-lighting)
 
 (provide 'theme)
 
