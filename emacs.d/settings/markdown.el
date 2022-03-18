7ef56fa213e5f9d7ed451a9f14638701420a34e2
Jandrolo <alejandro@ideasftw.com>
2022-03-18 17:47:35 -0400

    Disable emacs livedown

--- emacs.d/settings/markdown.el
+++ emacs.d/settings/markdown.el
@@ -7,19 +7,19 @@
 (use-package markdown-mode
   :ensure t)
 
-(use-package livedown
-  :config
-  (defvar livedown-autostart)
-  (setq livedown-autostart t))
+;; (use-package livedown
+;;   :config
+;;   (defvar livedown-autostart)
+;;   (setq livedown-autostart t))
 
-(define-minor-mode livedown-mode
-  "Override markdown mode to use livedown for preview"
-  :lighter " livedown"
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c C-c p") 'livedown-preview)
-            map))
+;; (define-minor-mode livedown-mode
+;;   "Override markdown mode to use livedown for preview"
+;;   :lighter " livedown"
+;;   :keymap (let ((map (make-sparse-keymap)))
+;;             (define-key map (kbd "C-c C-c p") 'livedown-preview)
+;;             map))
 
-(add-hook 'markdown-mode-hook 'livedown-mode)
+;; (add-hook 'markdown-mode-hook 'livedown-mode)
 
 (provide 'markdown)
 
