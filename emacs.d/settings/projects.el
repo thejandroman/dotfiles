6e7ebf6e8a7f151ef029340a001a1a77fc72190b
Han Drolo <alejandro@ideasftw.com>
2023-03-31 11:23:21 -0400

    Allow emacs neotree resize

--- emacs.d/settings/projects.el
+++ emacs.d/settings/projects.el
@@ -12,7 +12,19 @@
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   (defvar neo-vc-integration)
   (setq neo-vc-integration '(char))
+  (setq neo-window-fixed-size nil)
   )
+
+;; Set the neo-window-width to the current width of the
+;; neotree window, to trick neotree into resetting the
+;; width back to the actual window width.
+;; Fixes: https://github.com/jaypei/emacs-neotree/issues/262
+(eval-after-load "neotree"
+  '(add-to-list 'window-size-change-functions
+                (lambda (frame)
+                  (let ((neo-window (neo-global--get-window)))
+                    (unless (null neo-window)
+                      (setq neo-window-width (window-width neo-window)))))))
 
 (use-package projectile
   :ensure t)
